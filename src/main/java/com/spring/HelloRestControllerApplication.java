b67a23599a8047d02c8ea8b3492f2f5132e713ee
Ashok Mane <ashokmane81800@gmail.com>
2021-06-16 11:44:39 +0530

    [ashok] added restcontrooller API calls using Curlto print message

---
+++ src/main/java/com/spring/HelloRestControllerApplication.java
@@ -0,0 +1,25 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestControllerApplication.class, args);
+	}
+	
+	@RestController
+	public class HelloController {
+	 
+	    @RequestMapping("/hello")
+	    public String hello() {
+	        return "Hello From Bridgelbz";
+	    }  
+	}
+
+}
